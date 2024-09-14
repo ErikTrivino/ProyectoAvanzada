15c961fd4ec934a7d98946b5ee30b20ca775eebf
Adriana Garcia Garaviz <111439032+AdrianaGarcia1501@users.noreply.github.com>
2024-09-13 22:55:09 -0500

    implementacion CuentaServicio
    Avances en la implementación de CuentaServicio, se desarrolla método crearEvento.

--- src/main/java/co/edu/uniquindio/proyecto/modelo/dto/evento/EditarEventoDTO.java
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/dto/evento/EditarEventoDTO.java
@@ -1,4 +1,23 @@
 package co.edu.uniquindio.proyecto.modelo.dto.evento;
 
-public record EditarEventoDTO() {
+import co.edu.uniquindio.proyecto.modelo.enums.EstadoEvento;
+import co.edu.uniquindio.proyecto.modelo.enums.TipoEvento;
+import co.edu.uniquindio.proyecto.modelo.vo.Localidad;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record EditarEventoDTO(
+
+         @NotBlank String imagenPortada,
+         @NotBlank EstadoEvento estado,
+         @NotBlank String nombre,
+         @NotBlank String descripcion,
+         @NotBlank String imagenLocalidades,
+         @NotBlank TipoEvento tipo,
+         @NotBlank LocalDateTime fechaEvento,
+         @NotBlank String ciudad
+
+) {
 }
