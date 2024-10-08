fab66240f3a7e876eb15f4a23682eef725378997
cos-03 <112827893+cos-03@users.noreply.github.com>
2024-10-07 22:20:07 -0500

    no fui capaz muchachos

---
+++ src/test/CarritoServiceTest.java
@@ -0,0 +1,13 @@
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CarritoServiceTest {
+
+    @Test
+    void pruebaSencilla() {
+        assertEquals(2, 1 + 1, "1 + 1 debe ser igual a 2");
+    }
+}
+
